3c47462fab16b18d1be5bdd6e6d7864c5e16acdd
Erick Records <=>
2024-04-12 10:42:57 -0600

    Import firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,24 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { initializeApp, provideFirebaseApp, } from '@angular/fire/app';
+import { environment } from 'src/environments/environment.prod';
+
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB3wXlCTk96gO2nqfoZ8tUPY5xShR8E3t0",
+    authDomain: "eritasks.firebaseapp.com",
+    projectId: "eritasks",
+    storageBucket: "eritasks.appspot.com",
+    messagingSenderId: "483856270180",
+    appId: "1:483856270180:web:054be03acac61f3a3a3071",
+    measurementId: "G-0XHRY38F9M"
+  }
 };
